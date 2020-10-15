f7f4d6f806650b2c1c0924574c0e1613056b5297
Navneet Singh Gill <navneet105gill@gmail.com>
2020-10-15 11:33:19 -0400

    remove unwanted code and add scroll to popup

--- contentScripts/amazon.js
+++ contentScripts/amazon.js
@@ -1,6 +1,6 @@
-console.log("Amazon");
+console.log("amazon.js");
 
-console.log("a" + document.getElementById("ASIN") + "a")
+console.log("ASIN document", document.getElementById("ASIN"))
 if(document.getElementById("ASIN") != null){
     chrome.runtime.sendMessage({
         "message": "new_asin",
@@ -8,67 +8,16 @@ if(document.getElementById("ASIN") != null){
     });
 }
 
-// var port = chrome.runtime.connect({name: "knockknock"});
-// port.onMessage.addListener(function(message) {
-//     console.log("amazon: ", message)
-//   if (message.document != undefined)
-//     port.postMessage({
-//         "message": "new_asin",
-//         "value": document.getElementById("ASIN").value
-//     });
-// });
-
-// chrome.runtime.onConnect.addListener(function(port) {
-//     console.log("amazon1: ", port)
-
-//     port.onMessage.addListener(function(request) {
-//     if (message.document != undefined)
-//         port.postMessage({
-//             "message": "new_asin",
-//             "value": document.getElementById("ASIN").value
-//         });
-//     });
-//   });
-
 chrome.runtime.onMessage.addListener(
     (message, sender, callback) => {
-        console.log("Amazon.js - On message: ", message);
+        console.log("amazon.js - On message: ", message);
         if(message != null) {
             if (message.document != undefined) {
-                let key = "new_asin"
-                let value = document.getElementById(message.document).value
-                // response({
-                //     key: value 
-                // });
-                // callback({
-                //     "message": "new_asin",
-                //     "value": document.getElementById("ASIN").value
-                // })
                 chrome.runtime.sendMessage({
                     "message": "new_asin",
-                    "value": document.getElementById("ASIN").value
+                    "value": document.getElementById(message.document).value
                 });
             }
         }
-    })
-
-// traverse(myTab)
-// fetch('https://gist.githubusercontent.com/NavneetSinghGill/e38b6c60bc499d70b805f0e034cf0b0e/raw/f67adcf069264eba4fd8d467b993f0619f76ff0e/stores')
-//   .then(
-//     function(response) {
-//         if (response.status !== 200) {
-//         console.log('Looks like there was a problem. Status Code: ' +
-//           response.status);
-//         return;
-//       }
-
-//       // Examine the text in the response
-//       response.json().then(function(data) {
-//         console.log(data);
-//         chrome.runtime.sendMessage({type: "popup_info"});
-//       });
-//     }
-//   )
-//   .catch(function(err) {
-//     console.log('Fetch Error :-S', err);
-//   });
+    }
+)
