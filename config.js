66b4c5dace999317eec25dc9e5ef3cdfc5eaaa9f
Navneet Singh Gill <navneet105gill@gmail.com>
2020-10-30 11:16:27 -0400

    set azure deployment url

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 var config =  {}
 
-config.endpoint = "https://still-cove-55771.herokuapp.com"
+config.endpoint = "https://interfaceendpoint.azurewebsites.net"
+// config.endpoint = "https://still-cove-55771.herokuapp.com"
 // config.endpoint = "http://localhost:3000";
 
 export default config;
